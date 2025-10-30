f2f556443a6deb139d69b8caa70489b9d8b3053c
michalKopec <michal.apple@gmail.com>
2025-10-30 12:42:46 -0700

    Milestone 5 | Refactor restaurant app to use dynamic rendering
    Menu and order summary are now generated dynamically from data.js, replacing hardcoded HTML.
    
    Added modular functions for rendering menu items, handling order updates, and displaying order confirmation.
    
    Improved form accessibility and styling

--- 07-essential-javascript/05-solo-restaurant-ordering-app/index.js
+++ 07-essential-javascript/05-solo-restaurant-ordering-app/index.js
@@ -1,36 +1,117 @@
-/* main event loop */ 
+import { menuArray } from "./data.js"
+const menuEl = document.getElementById("menu-feed"); 
+const orderEl = document.getElementById("order-items"); 
+const totalPriceEl = document.getElementById("text-price"); 
+const orderConfirmationEl = document.getElementById("order-confirmation"); 
+let totalPrice = 0; 
+
+// Main event loop 
+ 
 document.addEventListener('click', function(e) {
-    console.log(e.target.classList)
     
     if (e.target.dataset.add){
-       renderOrderSummary(e.target.dataset.add)
+        document.querySelector(".order-summary").style.display="block";
+        getOrderHTML(e.target.dataset.add)
     }
     else if(e.target.dataset.remove) {
-        removeFromOrder(e.target.dataset.remove)
+        removeItemAndUpdateTotal(e.target)
     }
     else if (e.target.id === "order-btn") {
-        completeOrder(e.target.id)
-    }
-    else if (e.target.id === "modal-pay-btn") {
-        payForOrder(e.target.id);
+        showEnterCardDetails()
     }
 })
 
-function completeOrder(targetID) {
-    console.log('from continue order ',targetID)
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    orderConfirmation(document.querySelector('#name').value)
+}) 
+
+// Create food menu HTML
+
+function getMenuHTML() {
+    let menuHTML = ''; 
+    menuArray.forEach(function(item){
+        menuHTML += `
+            <article class="menu-item">
+                <img class="item-picture" src="/images/${item.name}-item.png" alt="${item.alt}">
+                <ul class="item-details">
+                    <li class="item-name text-heading">${item.name}</li>
+                    <li class="item-ingredients text-subheading">${item.ingredients}</li>
+                    <li class="item-price text-price">$${item.price}</li>
+                </ul>
+            <button class="item-add-btn text-icon" data-add="${item.id}">+</button>
+            </article>
+        ` 
+    }) 
+    return menuHTML; 
+}
+
+// Create order 
+
+function getOrderHTML(itemNumber) {
+    const orderItem = findItem(itemNumber); 
+    
+    let orderList = ''
+    
+    orderList = `
+        <div class="order-item">
+            <dt class="text-heading">${orderItem.name}</dt>
+            <button class="text-secondary-sm" type="button" aria-label="Remove ${orderItem.name}" data-remove="${orderItem.id}">remove</button>
+            <dd class="text-price">$${orderItem.price}</dd>
+        </div>
+    `
+    orderEl.innerHTML += orderList;
+    updateOrderTotal(orderItem.price); 
+}
+
+// Find which item add to order 
+
+function findItem(itemNumber) {
+    const orderItem = menuArray.filter(function(item){
+        return +itemNumber === item.id
+     })[0]; 
+     
+    return orderItem; 
+}
+
+// Remove item from order 
+
+function removeItemAndUpdateTotal(row) {
+    row.parentElement.remove();
+    
+    //find the price of the removed item, then subtract its value from total  
+    updateOrderTotal(-`${findItem(row.dataset.remove).price}`) 
+}
+
+
+// Update order total as the items are added or removed 
+
+function updateOrderTotal(price) {
+    totalPrice += +price; 
+    totalPriceEl.textContent = `$${totalPrice.toString()}`; 
+}
+
+
+// Complete order 
+
+function showEnterCardDetails() {
     document.querySelector("#modal").style.display="block";
+    document.querySelector("#order-summary").style.display="none";
 }
 
+// Modal: pay for order and display confirmation 
 
-function payForOrder(targetID) {
-    console.log('from pay for order ',targetID)
+function orderConfirmation(name) {
     document.querySelector("#modal").style.display="none";
-    document.querySelector(".order-summary").style.display="none";
-    document.querySelector(".order-confirmation").style.display="block";
+    document.querySelector("#order-confirmation").style.display="block";
+    
+    orderConfirmationEl.innerHTML = `<p class="text-heading">Thanks, ${name}! Your order is on its way!</p>`
 }
 
-function renderOrderSummary(add) {
-    if (add === "id1") {
-        document.querySelector(".order-summary").style.display="block";
-    }
+// Render menu items 
+
+function render() {
+    menuEl.innerHTML = getMenuHTML();
 }
+
+render(); 
